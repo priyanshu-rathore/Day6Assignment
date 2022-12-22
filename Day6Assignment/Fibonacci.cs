e7559b626abf66cb9306638fe78c413a31a2fa79
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-22 18:51:03 +0530

    Fibonacci Program

---
+++ Day6Assignment/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment
+{
+    internal class Fibonacci
+    {
+        public static void fibonacci()
+        {
+            Console.WriteLine("Enter the Number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            arr[0] = 0;
+            arr[1] = 1;
+
+            for(int i = 2; i < n; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]+ " ");
+            }
+
+        }
+    }
+}
---
+++ Day6Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day6Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1). Fibonacci series");
+            int option = Convert.ToInt32(Console.ReadLine());   
+
+            switch(option)
+            {
+                case 1:Fibonacci.fibonacci();
+                    break;
+            }
+        }
+    }
+}
