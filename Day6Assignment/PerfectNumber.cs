57a084538c575902f108f9293b830e45954d5110
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-22 19:06:27 +0530

    Perfect Numbers Program

---
+++ Day6Assignment/PerfectNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment
+{
+    internal class PerfectNumber
+    {
+        public static void perfectNumber()
+        {
+            Console.Write("Enter Number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for(int i=1;i<=n/2;i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if(sum==n)
+            {
+                Console.WriteLine(n + " is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(n+ "is not a perfect number");
+            }
+        }
+    }
+}
--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -4,13 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1). Fibonacci series");
+            Console.WriteLine("1). Fibonacci series  2). Perfect Number");
             int option = Convert.ToInt32(Console.ReadLine());   
 
             switch(option)
             {
                 case 1:Fibonacci.fibonacci();
                     break;
+                case 2:PerfectNumber.perfectNumber(); 
+                    break;
+
+
             }
         }
     }
